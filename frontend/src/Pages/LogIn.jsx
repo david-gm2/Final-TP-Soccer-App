18c65fad9850fc51b5736292f7822ad8e17e83e4
Ivan-gauto-e <ivan.gauto@gm2dev.com>
2025-11-10 10:32:07 -0300

    Implement login functionality with error handling and user feedback

---
+++ frontend/src/Pages/LogIn.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMesagge] = useState("");
+
+
+const handleSubmit = aysnc (e) => {
+    e.preventdefault();
+
+if (!email || !password) {
+    setMesagge("Faltan campos obligatorios.");
+    return;
+}
+      try {
+    const response = await fetch(
+      "https://backend-exercises-production.up.railway.app/auth/login",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      }
+    );
+    const data = await response.json();
+    if(!response.ok) throw new Error(data.mesagge || "esta mal xd");
+    localStorage.setItem("accesToken", data.accesToken)
+    setMesagge("Login exitoso, redirigiendo a la pagina...")
+    setTimeout(()=> {
+        window.location.href = "/visitante"
+      }, 1500)
+} catch (error){
+    setMesagge(`${error.mesagge}`)
+}
+}}
