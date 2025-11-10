cb50860b4774819c03ec3cfca181141bc5eadc86
Ivan-gauto-e <ivan.gauto@gm2dev.com>
2025-11-10 10:53:14 -0300

    Fix login form submission and message handling

--- frontend/src/Pages/LogIn.jsx
+++ frontend/src/Pages/LogIn.jsx
@@ -3,44 +3,46 @@ import { useState } from "react";
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [message, setMesagge] = useState("");
+  const [message, setMessage] = useState("");
 
+  const handleSubmit = async (e) => {
+    e.preventdDefault();
 
-const handleSubmit = aysnc (e) => {
-    e.preventdefault();
+    if (!email || !password) {
+      setMessage("Faltan campos obligatorios.");
+      return;
+    }
+    try {
+      const response = await fetch(
+        "https://backend-exercises-production.up.railway.app/auth/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, password }),
+        }
+      );
+      const data = await response.json();
+      if (!response.ok) throw new Error(data.message || "esta mal xd");
+      localStorage.setItem("accessToken", data.accessToken);
+      setMessage("Login exitoso, redirigiendo a la pagina...");
+      setTimeout(() => {
+        window.location.href = "/visitante";
+      }, 1500);
+    } catch (error) {
+      setMessage(`${error.message}`);
+    }
+  };
 
-if (!email || !password) {
-    setMesagge("Faltan campos obligatorios.");
-    return;
-}
-      try {
-    const response = await fetch(
-      "https://backend-exercises-production.up.railway.app/auth/login",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      }
-    );
-    const data = await response.json();
-    if(!response.ok) throw new Error(data.mesagge || "esta mal xd");
-    localStorage.setItem("accesToken", data.accesToken)
-    setMesagge("Login exitoso, redirigiendo a la pagina...")
-    setTimeout(()=> {
-        window.location.href = "/visitante"
-      }, 1500)
-} catch (error){
-    setMesagge(`${error.mesagge}`)
-}
-}
-return (
+  return (
     <div className="loginContainer">
-        <div className="loginCard">
-            <h2>"Log In</h2>
-            <p className="subtitle">
-                Connect players. Create matches. <br />
-                    Keep the game alive.</p>
-        </div>
+      <div className="loginCard">
+        <h2>"Log In</h2>
+        <p className="subtitle">
+          Connect players. Create matches. <br />
+          Keep the game alive.
+        </p>
+        <form onSubmit={handleSubmit}></form>
+      </div>
     </div>
-)
+  );
 }
