b444a77bf33d2caeabf8fe83197adce0f69391df
diegocroquer-arch <diego.croquer@gm2dev.com>
2025-11-11 12:33:06 -0300

    feat: implement dynamic user stats in Home component

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,11 +1,17 @@
 import "../styles/home.css";
 
 function Home() {
+  let user = "user";
+  let totalMatches = 5;
+  let totalGoals = 44;
+  let totalAssists = 13;
+  let activePlayers = 20;
+
   return (
     <main className="home-page">
       <div className="welcome-section">
         <div>
-          <h1>Welcome [user]! </h1>
+          <h1>Welcome {user}! </h1>
           <p>
             Here’s your weekly summary with the key stats and upcoming matches.
           </p>
@@ -25,31 +31,31 @@ function Home() {
         <div className="stats-card">
           <div>
             <h2>Total Matches</h2>
-            <p>5</p>
+            <p>{totalMatches}</p>
           </div>
 
           <img src="../public/icons/scoreboard.svg" alt="" />
         </div>
         <div className="stats-card">
           <div>
-            <h2>Total Matches</h2>
-            <p>44</p>
+            <h2>Total Goals</h2>
+            <p>{totalGoals}</p>
           </div>
 
           <img src="../public/icons/sports_soccer.svg" alt="" />
         </div>
         <div className="stats-card">
           <div>
-            <h2>Total Matches</h2>
-            <p>13</p>
+            <h2>Total assists</h2>
+            <p>{totalAssists}</p>
           </div>
 
           <img src="../public/icons/star.svg" alt="" />
         </div>
         <div className="stats-card">
           <div>
-            <h2>Total Matches</h2>
-            <p>20</p>
+            <h2>Active Players</h2>
+            <p>{activePlayers}</p>
           </div>
 
           <img src="../public/icons/person.svg" alt="" />
