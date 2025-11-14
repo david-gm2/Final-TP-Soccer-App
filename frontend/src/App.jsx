612d48e21b4c288355a1f2c36c3fc3e00fcfa3f1
david.alvarez <david.alvarez@gm2dev.com>
2025-11-14 09:57:47 -0300

    fix: corregir la estructura de rutas en App.jsx y eliminar líneas en blanco innecesarias

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
-
 import { Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar.jsx";
 import SignUp from "./Pages/PageSignUp.jsx";
 import NotFoundPage from "./Pages/PageNotFound.jsx";
 import StatsPage from "./Pages/PageStats.jsx";
+import Home from "./Pages/Home.jsx";
 import MatchesPage from "./Pages/PageMatches.jsx";
 import PlayersPage from "./Pages/PagePlayers.jsx";
 import HistoryPage from "./Pages/PageHistory.jsx";
 import LogIn from "./Pages/PageLogIn.jsx";
-
 import "./App.css";
-
 
 const dummyPlayer = {
   name: "Martínez",
@@ -23,7 +21,6 @@ const dummyPlayer = {
   azul: 50,
 };
 
-
 function App() {
   return (
     <Routes>
@@ -31,14 +28,15 @@ function App() {
       <Route path="/log-in" element={<LogIn />} />
 
       <Route path="/" element={<Sidebar />}>
-        <Route path="/stats" element={<StatsPage player={dummyPlayer} />} />
-        <Route path="/matches" element={<MatchesPage />} />
-        <Route path="/players" element={<PlayersPage />} />
-        <Route path="/players/:id" element={<PlayersPage />} />
-        <Route path="/history" element={<HistoryPage />} />
+        <Route index element={<Home />} />
+        <Route path="stats" element={<StatsPage player={dummyPlayer} />} />
+        <Route path="matches" element={<MatchesPage />} />
+        <Route path="players" element={<PlayersPage />} />
+        <Route path="players/:id" element={<PlayersPage />} />
+        <Route path="history" element={<HistoryPage />} />
       </Route>
 
-      <Route path="*" elementa={<NotFoundPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
