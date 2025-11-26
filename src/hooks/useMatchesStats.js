0c9e45032f2b293e46e3cac0cd9f69918eec512d
cesargm2 <cesar.kastli@gm2dev.com>
2025-11-26 13:13:08 -0300

    feat: add home matches stats (#35)

---
+++ src/hooks/useMatchesStats.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { API_BACKEND_URL } from "../constants/API_CONSTANTS.js";
+import { authFetch } from "../utils/authFetch.js";
+
+export const useMatchesStats = () => {
+  const [stats, setStats] = useState(null);
+
+  useEffect(() => {
+    const fetchStats = async () => {
+      const response = await authFetch(`${API_BACKEND_URL}/matches/stats`);
+      if (!response.ok) {
+        return;
+      }
+      const data = await response.json();
+      setStats(data);
+    };
+
+    fetchStats();
+  }, []);
+
+  return stats;
+};
